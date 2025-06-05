7b8ae3ca3901410614a51b461dc96d211c517f13
notrodi <nnotrodi1@gmail.com>
2025-06-05 20:51:25 +0300

    updated data for analytics

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,19 @@
+import { useSelector } from 'react-redux';
 import MainAccount from '../../components/MainAccount/MainAccount';
 import CategoryPieChart from '../../components/PieChart/CategoryPieChart';
 import { getChartData } from '../../functions';
-import { transactionsMock } from '../../mocks';
+import type { RootState } from '../../store/store';
 
 export default function HomePage() {
+  const transactions = useSelector((state: RootState )=> state.transactions);
+  
   return (
     <div className='page-container'>
         <h2 className='main-title'>Главная</h2>
 
         <MainAccount />
         
-        <CategoryPieChart data={ getChartData(transactionsMock) }/>
+        <CategoryPieChart data={ getChartData(transactions) }/>
     </div>
   )
 }

2dcedf0e5bbd8a9396acd87d0475ddf44d18cdb2
notrodi <nnotrodi1@gmail.com>
2025-06-13 17:09:22 +0300

    added chart plug

--- src/components/PieChart/CategoryPieChart.tsx
+++ src/components/PieChart/CategoryPieChart.tsx
@@ -2,6 +2,18 @@ import { PieChart, Pie, Cell } from 'recharts';
 import { formatMoney } from '../../functions';
 import './CategoryPieChart.scss';
 
+const CHART_PLUG = [
+  {
+    value: 30
+  },
+  {
+    value: 60
+  },
+  {
+    value: 10
+  }
+]
+
 type PieChartDataItem = {
   title: string;
   color: string;
@@ -16,10 +28,10 @@ export default function CategoryPieChart({ data }: PieChartProps) {
   return (
     <div className='category-pie-chart'>
       <h3 className='second-title'>Аналитика</h3>
-
+      
       <PieChart width={240} height={240}>
         <Pie
-          data={data}
+          data={ data.length ? data : CHART_PLUG }
           dataKey="value"
           cx="50%"
           cy="50%"
@@ -35,24 +47,27 @@ export default function CategoryPieChart({ data }: PieChartProps) {
       </PieChart>
 
       <ul className='category-pie-chart__legend'>
-        {data.map((item, index) => (
-          <li className='category-pie-chart__legend__item' key={`item-${index}`}>
-            <div className='category-pie-chart__legend__item__category'>
-              <span
-                style={{
-                  display: 'inline-block',
-                  width: 10,
-                  height: 10,
-                  backgroundColor: item.color,
-                  borderRadius: '50%',
-                  marginRight: 8,
-                }}
-              />
-              <div>{item.title}</div>
-            </div>
-            <div>{`${formatMoney(item.value)} ₽`}</div>
-          </li>
-        ))}
+        {
+          data.length ?
+          data.map((item, index) => (
+            <li className='category-pie-chart__legend__item' key={`item-${index}`}>
+              <div className='category-pie-chart__legend__item__category'>
+                <span
+                  style={{
+                    display: 'inline-block',
+                    width: 10,
+                    height: 10,
+                    backgroundColor: item.color,
+                    borderRadius: '50%',
+                    marginRight: 8,
+                  }}
+                />
+                <div>{item.title}</div>
+              </div>
+              <div>{`${formatMoney(item.value)} ₽`}</div>
+            </li> )) :
+          <div className='category-pie-chart__plug-text'>Нет данных</div>
+        }
       </ul>
     </div>
   );
